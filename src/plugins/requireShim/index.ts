5a8c93825f860dd0c20533855fa6336e965616e5
C. Lewis <ctj.lewis@icloud.com>
2021-09-05 00:09:01 -0500

    v0.2.3

--- src/plugins/requireShim/index.ts
+++ src/plugins/requireShim/index.ts
@@ -5,19 +5,17 @@ const REQUIRE_SHIM = `import{require}from'@tszip/esm-require';`;
 export const requireShim = (): Plugin => ({
   name: 'Shim require().',
   renderChunk: async (code) => {
-    if (code.includes('require(') || code.includes('require.')) {
-      let banner = REQUIRE_SHIM;
-      if (code.startsWith('#!')) {
-        const afterNewline = code.indexOf('\n') + 1;
-        const shebang = code.slice(0, afterNewline);
-        code = code.slice(afterNewline);
-        banner = shebang + REQUIRE_SHIM;
-      }
-      code = banner + code;
+    if (code.startsWith('#!')) {
+      const afterNewline = code.indexOf('\n') + 1;
+      const shebang = code.slice(0, afterNewline);
+      return {
+        code: shebang + REQUIRE_SHIM + code.slice(afterNewline),
+        map: null
+      };
     }
 
     return {
-      code,
+      code: REQUIRE_SHIM + code,
       map: null,
     };
   },
