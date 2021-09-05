73ea9058e9229d43be9924eb848f8e4a2307de53
C. Lewis <ctj.lewis@icloud.com>
2021-09-05 00:22:44 -0500

    chore: add simple TSC plugin for later

---
+++ src/plugins/tsc/index.ts
@@ -0,0 +1,13 @@
+/**
+ * This simply runs `tsc` in process.cwd(), reading the TSConfig in that
+ * directory, and forcing an emit.
+ */
+// export default function simpleTS(...args: any[]) {
+//   return {
+//     name: 'Run TSC',
+//     /**
+//      * Wait for the process to finish.
+//      */
+//     buildStart: async () => await runTsc(...args),
+//   };
+// }
